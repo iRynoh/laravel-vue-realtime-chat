5b130df44dedc19629adb88abec7414aab549498
Percy Astocaza <percy0x@gmail.com>
2024-12-07 10:50:59 -0500

    Room controller, model and resource

---
+++ app/Http/Controllers/RoomShowController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RoomResource;
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomShowController extends Controller
+{
+    public function __invoke(Room $room)
+    {
+        return inertia()->render('Room', [
+            'room' => RoomResource::make($room)
+        ]);
+    }
+}
